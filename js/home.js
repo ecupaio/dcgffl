4f33ba5a177216f4dd015dce6bf090fd2dec1125
Ed Cupaioli <ecupaio@gmail.com>
2021-04-27 09:13:44 -0700

    news section

--- js/home.js
+++ js/home.js
@@ -7,28 +7,31 @@ $(function() {
       'api_key': 'b2e7ca6837ac851207f38ec05da28756',
       'user_id': '55392288@N03',
       'format': 'json',
-      'per_page': 5,
+      'per_page': 10,
       'page': 1,
       'nojsoncallback': 1
     },
     success: function(data) {
-      console.log(data);
+      
       var photos = data.photos.photo;
       $(photos).each(function(i,photo) {
         console.log(photo);
         var photoSlide = '<div class="photo-slide">'+
-                            '<img  src="https://live.staticflickr.com/'+photo.server+'/'+photo.id+'_'+photo.secret+'_b.jpg"/>'+
+                            '<img  src="https://live.staticflickr.com/'+photo.server+'/'+photo.id+'_'+photo.secret+'_b.jpg" alt="DCGFFL game photo" />'+
                           '<div>';  
         $('#photo-slider').append(photoSlide);
       });
       $('#photo-slider').slick({
         slidesToShow: 1,
         autoplay: true
-
       });
     },
     error: function(data) {
       console.log(data);
     }
+  });
+  $('.news-feed').slick({
+    slidesToShow: 3,
+    dots: true
   });
 });
