56c145157213c32d49f071f5b5deede1781d7028
Ed Cupaioli <ecupaio@gmail.com>
2021-06-09 10:56:15 -0700

    made menu fixed

--- js/site_functions.js
+++ js/site_functions.js
@@ -2,25 +2,6 @@ $(function(){
   $('.nav-toggle').click(function() {
     $('nav,html').toggleClass('active');
   });
-  $(window).on('scroll load resize',function() {
-    var scrollTop;
-    if (inView('nav') == false) {
-      $('nav').addClass('sticky');
-      scrollTop = $(window).scrollTop();
-      $('main').addClass('stick-nav');
-    }
-    if ($(window).scrollTop() == 0) {
-      $('nav').removeClass('sticky');
-    } 
-  });
-  function inView(element) {
-    var elementTop = $(element).offset().top;
-    var elementBottom = elementTop + $(element).outerHeight();
-    var viewportTop = $(window).scrollTop();
-    var viewportBottom = viewportTop + $(window).height();
-    return elementBottom > viewportTop && elementTop < viewportBottom;
-  };
-    
   $('.close-announcement').click(function() {
     $('.announcement-topbar').slideUp();
     var d = new Date();
