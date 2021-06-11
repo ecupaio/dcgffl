cfe0471587c642f1a3988d26780b169bc8d50842
Ed Cupaioli <ecupaio@gmail.com>
2021-06-11 11:57:13 -0700

    rules page

--- js/rules.js
+++ js/rules.js
@@ -1,6 +1,8 @@
 $(function() {
-  $('.rule-title').click(function() {
+  $('.rule-title').click(function(e) {
+    e.preventDefault();
     $(this).parents('.rule').find('.rule-text').slideToggle();
-    $(this).toggleClass('active');
+    $(this).parents('.rule').toggleClass('active');
+  
   });
 });
