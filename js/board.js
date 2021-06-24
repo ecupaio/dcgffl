d22b767914611af7f6d7d33bcbb30c8b298d8e38
Ed Cupaioli <ecupaio@gmail.com>
2021-06-24 12:05:52 -0700

    added candidates to board page

--- js/board.js
+++ js/board.js
@@ -4,4 +4,12 @@ $(function() {
     $(this).next('.minutes-list').slideToggle();
     $(this).toggleClass('active');
   });
-})
+  $('.statement-toggle').click(function() {
+    $(this).next('.statement-overlay').addClass('active');
+    $('html,body').addClass('active');
+  });
+  $('.close-statement-overlay').click(function() {
+    $(this).parents('.statement-overlay').removeClass('active');
+    $('html,body').removeClass('active');
+  });
+}); 
